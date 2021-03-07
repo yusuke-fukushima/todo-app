df28b3d845af30e8e626233a3fe042fc1d66041a
yusuke-fukushima <usk.fshima.1994@gmail.com>
2021-03-07 11:36:44 +0900

    投稿機能の作成

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from datetime import datetime
+
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -12,10 +14,28 @@ class Post(db.Model):
   due = db.Column(db.DateTime, nullable=False)
 
 
-@app.route('/')
+@app.route('/', methods= ['GET', 'POST'])
 def index():
-  return render_template('index.html')
+  if request.method == 'GET':
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
+  else:
+    title = request.form.get('title')
+    detail = request.form.get('detail')
+    due = request.form.get('due')
 
+    due = datetime.strptime(due, '%Y-%m-%d')
+    new_post = Post(title=title, detail=detail, due=due)
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect('/')
+
+
+@app.route('/create')
+def create():
+  return render_template('create.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
